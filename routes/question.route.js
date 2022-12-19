6aa938e95fb49693cfc904b01fa3f24086b808eb
sonlam <19522133@gm.uit.edu.vn>
2022-12-19 19:33:28 +0700

    change rule get question

--- routes/question.route.js
+++ routes/question.route.js
@@ -4,7 +4,7 @@ const questionCtrl = require('../controller/question.controller')
 const verifyToken = require('../middleware/auth.middleware');
 
 router.post('/', questionCtrl.getQuestions);
-router.post('/random-question', verifyToken, questionCtrl.getRandomQuestion);
+router.post('/random-question', questionCtrl.getRandomQuestion);
 router.post('/create', questionCtrl.createQuestion);
 router.post('/update', questionCtrl.updateQuestion);
 router.post('/delete', questionCtrl.deleteQuestion);
